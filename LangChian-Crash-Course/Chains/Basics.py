f5b9f9e9a22c45c41918aad0d7879a8f266e4ced
Samitha10 <samithabesinghe1999@gmail.com>
2024-07-04 18:40:14 +0530

    feat: Add LangChian Crash Course code files and dependencies

--- LangChian-Crash-Course/Chains/Basics.py
+++ LangChian-Crash-Course/Chains/Basics.py
@@ -1,8 +1,28 @@
 from langchain_groq import ChatGroq
 import os
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import HumanMessage
 
 groq_key = os.environ.get("GROQ_KEY")
 
 # Create a ChatGroq model
 model = ChatGroq(model='mixtral-8x7b-32768', api_key=groq_key)
 
+# Define prompt templates (no need for separate Runnable chains)
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a comedian who tells jokes about {topic}."),
+        ("human", "Tell me {joke_count} jokes."),
+    ]
+)
+
+# Create the combined chain using LangChain Expression Language (LCEL)
+chain = prompt_template | model | StrOutputParser()
+# chain = prompt_template | model
+
+# Run the chain
+result = chain.invoke({"topic": "lawyers", "joke_count": 3})
+
+# Output
+print(result)
