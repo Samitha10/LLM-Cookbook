5800664c60d9b498371221592bcaaf435598f64e
Samitha10 <samithabesinghe1999@gmail.com>
2024-07-04 20:37:21 +0530

    chore: Add LangChian Crash Course code files and dependencies

---
+++ LangChian-Crash-Course/Chains/under_hood.py
@@ -0,0 +1,33 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnableSequence
+from langchain_groq import ChatGroq
+
+# Load environment variables from .env
+import os
+
+groq_key = os.environ.get("GROQ_KEY")
+
+# Create a ChatGroq model
+model = ChatGroq(model='mixtral-8x7b-32768', api_key=groq_key)
+
+# Define prompt templates
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a comedian who tells jokes about {topic}."),
+        ("human", "Tell me {joke_count} jokes."),
+    ]
+)
+
+# Create individual runnables (steps in the chain)
+format_prompt = RunnableLambda(lambda x: prompt_template.format_prompt(**x))
+invoke_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
+parse_output = RunnableLambda(lambda x: x.content)
+
+# Create the RunnableSequence (equivalent to the LCEL chain)
+chain = RunnableSequence(first=format_prompt, middle=[invoke_model], last=parse_output)
+
+# Run the chain
+response = chain.invoke({"topic": "lawyers", "joke_count": 3})
+
+# Output
+print(response)
